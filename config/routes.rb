749f2239d619db7efe064f1ee7a55058dc2accb9
安達和史 <kazu40808@gmail.com>
2017-06-03 12:50:50 +0900

    シンボルスペース

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'groups#index'
-  resources :groups, only:[:index] do
-    resources :messages, only:[:index]
+  resources :groups, only: [:index] do
+    resources :messages, only: [:index]
   end
 end
