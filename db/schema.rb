1927533b78243040b3d9f45c8fe9f7266c6437c2
安達和史 <kazu40808@gmail.com>
2017-06-08 23:57:35 +0900

    create member model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608042840) do
+ActiveRecord::Schema.define(version: 20170608144241) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       default: "", null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "group_id_id", null: false
+    t.integer  "user_id_id",  null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["group_id_id"], name: "index_members_on_group_id_id", using: :btree
+    t.index ["user_id_id"], name: "index_members_on_user_id_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
