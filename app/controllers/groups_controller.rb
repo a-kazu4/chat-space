44b4f885f5db507629066b590a131ddb697e86b4
安達和史 <kazu40808@gmail.com>
2017-06-12 16:23:32 +0900

    use save method groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,8 +10,9 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.create(group_params)
-    if @group
+    @group = Group.new(group_params)
+    @group.save
+    if @group.save
       redirect_to root_path, notice: 'グループが作成されました。'
     end
   end
