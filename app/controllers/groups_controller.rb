8a9fcb8d70e7a15aeaa8881ca86b3fcefff05b4b
安達和史 <kazu40808@gmail.com>
2017-06-12 13:45:20 +0900

    create group creating function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,18 +1,31 @@
 class GroupsController < ApplicationController
 
   def index
+    @groups = Group.order('updated_at DESC')
   end
 
   def new
+    @group = Group.new
+    @users = User.all
   end
 
   def create
+    @group = Group.create(group_params)
+    if @group
+      redirect_to root_path, notice: 'グループが作成されました。'
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, {user_ids:[]})
   end
 
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :members
-end
+  accepts_nested_attributes_for :members
+  validates :name, presence: true
+  end
