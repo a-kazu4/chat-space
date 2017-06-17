5612e9c557a4e70bb4d213c212321735a3ac9a4e
安達和史 <kazu40808@gmail.com>
2017-06-17 12:37:31 +0900

    change ja.yaml

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,10 +2,5 @@ class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :members
   accepts_nested_attributes_for :members
-  validate :add_error_name
-  def add_error_name
-    if name.blank?
-      errors[:base] << "グループ名を入力して下さい"
-    end
-  end
+  validates :name, presence: true
 end
