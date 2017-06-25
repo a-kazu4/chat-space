03a42f3943dbec35fd24f853d7e7c2c5888a717c
安達和史 <kazu40808@gmail.com>
2017-06-25 16:21:45 +0900

    define action on messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,28 @@
 class MessagesController < ApplicationController
+
   def index
+    @groups = Group.all
+    @group = Group.find(params[:group_id])
+    @users = @group.users
+    @message = Message.new
+    @messages = @group.messages
   end
+
+  def create
+    @message = Message.new(message_params)
+    @group = Group.find(params[:group_id])
+    if @message.save
+      redirect_to group_messages_url(@group)
+    else
+      redirect_to group_messages_url(@group), alert: 'メッセージを入力してください。'
+    end
+  end
+
+  private
+
+  def message_params
+    group = Group.find(params[:group_id])
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: group.id)
+  end
+
 end
