3fa209c0f37421fdf1e36f58fb54bfd2ee1730b6
安達和史 <kazu40808@gmail.com>
2017-06-25 16:26:59 +0900

    validation :body, :image

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,10 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
   mount_uploader :image, MessageUploader
+  validates :body_or_image, presence: true
+
+  private
+  def body_or_image
+    body.presence or image.presence
+  end
 end
