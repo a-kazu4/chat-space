ea6d2364874443383894235a56f3f5a68c5b6e6a
安達和史 <kazu40808@gmail.com>
2017-06-25 10:40:45 +0900

    association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :members
+  has_many :messages
   accepts_nested_attributes_for :members
   validates :name, presence: true
   scope :updated_at, -> { order('updated_at DESC') }
