d244a2e0495d35790488c9cf5a3e8d9a3b101647
安達和史 <kazu40808@gmail.com>
2017-06-27 14:05:05 +0900

    insert blank line in models

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,10 @@ class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :members
   has_many :messages
+
   accepts_nested_attributes_for :members
+
   validates :name, presence: true
   scope :order_by_updated_at, -> { includes(:messages).order('updated_at DESC') }
+
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,10 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
   scope :order_by_updated_at, -> { includes(:messages).order('updated_at DESC') }
+
+
   mount_uploader :image, MessageUploader
+
   validates :body_or_image, presence: true
 
   private
