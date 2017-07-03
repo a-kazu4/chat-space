eff4cd8f017537f4150b7e393fadec40dc237bcd
安達和史 <kazu40808@gmail.com>
2017-07-03 22:16:20 +0900

    test message model

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+describe Message do
+  describe '#create' do
+  # メッセージを保存できる場合
+    # メッセージと画像があれば保存できる
+    it 'is valid with a body and an image' do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+
+    # メッセージがあれば保存できる
+    it 'is valid with a body' do
+      message = build(:message, image: '')
+      expect(message).to be_valid
+    end
+
+    # 画像があれば保存できる
+    it 'is valid with a image' do
+      message = build(:message, body: '')
+      expect(message).to be_valid
+    end
+
+  # メッセージを保存できる場合
+    # メッセージも画像も無いと保存できない
+    it 'is invalid without a body and an image' do
+      message = build(:message, body: '', image: nil)
+      message.valid?
+      expect(message.errors[:body_or_image]).to include('を入力してください。')
+    end
+
+    # group_idが無いと保存できない
+    it 'is invalid without a group_id' do
+      message = build(:message, group_id: '')
+      message.valid?
+      expect(message.errors[:group_id]).to include('を入力してください。')
+    end
+
+    # user_idが無いと保存できない
+    it 'is invalid without a user_id' do
+      message = build(:message, user_id: '')
+      message.valid?
+      expect(message.errors[:user_id]).to include('を入力してください。')
+    end
+
+  end
+end
