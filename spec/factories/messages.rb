d33be019b6730852b83ce8c6c573acabfc9fb5fd
安達和史 <kazu40808@gmail.com>
2017-07-04 11:03:35 +0900

    use faker

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
 
   factory :message do
-    body 'hello world'
+    body Faker::Pokemon.name
     image Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image/image.jpg'))
-    user_id 1
-    group_id 1
+    user_id Faker::Number.number(3)
+    group_id Faker::Number.number(3)
   end
 
 end
