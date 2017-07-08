75c864e2fc0371b5d4968a68eefdae8e69f28c0c
安達和史 <kazu40808@gmail.com>
2017-07-08 11:06:28 +0900

    modify message_spec.rb without destructive method

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,93 +2,130 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
     let(:message) { Message.new(message_params) }
+    let(:message_params) { { body: body, image: image, user_id: user_id, group_id: group_id } }
 
-    let(:message_params) do
-      hash = {}
-      hash[:body] = Faker::Pokemon.name
-      hash[:image] = Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image/image.jpg'))
-      hash[:user_id] = Faker::Number.number(3)
-      hash[:group_id] = Faker::Number.number(3)
-      hash
-    end
-
+    let(:text) { Faker::Pokemon.name }
+    let(:url) { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image/image.jpg')) }
     let(:num) { Faker::Number.number(10) }
 
     # メッセージを保存できる場合
     context 'if a message can be saved' do
+      subject { message }
 
       # メッセージと画像があれば保存できる
-      it 'is valid with a body and an image' do
-        expect(message).to be_valid
+      context 'with a message and an image' do
+        let(:body) { text }
+        let(:image) { url }
+        let(:user_id) { num }
+        let(:group_id) { num }
+        it { is_expected.to be_valid }
       end
 
       # メッセージがあれば保存できる
-      it 'is valid with a body' do
-        message_params.merge!(image: '')
-        expect(message).to be_valid
+      context 'with a body' do
+        let(:body) { text }
+        let(:image) { nil }
+        let(:user_id) { num }
+        let(:group_id) { num }
+        it { is_expected.to be_valid }
       end
 
       # 画像があれば保存できる
-      it 'is valid with a image' do
-        message_params.merge!(body: '')
-        expect(message).to be_valid
+      context 'with a image' do
+        let(:body) { '' }
+        let(:image) { url }
+        let(:user_id) { num }
+        let(:group_id) { num }
+        it { is_expected.to be_valid }
       end
 
       # 外部キー制約
+      context 'with FOREIGN KEY constraint' do
+        let(:body) { text }
+        let(:image) { url }
+        let(:user_id) { num }
+        let(:group_id) { num }
+
         # user_idと同じidを持つユーザーがいれば保存できる
-      it 'is valid with FOREIGN KEY constraint on user_id' do
-        message_params.merge!(user_id: num)
-        user = build(:user, id: num)
-        expect(message.user_id).to eq(user.id)
-      end
+        context 'on user_id if there is User who has a same id' do
+          it 'should be valid' do
+            user = build(:user, id: num)
+            expect(message.user_id).to eq(user.id)
+          end
+        end
 
         # group_idと同じidを持つユーザーがいれば保存できる
-      it 'is valid with FOREIGN KEY constraint on group_id' do
-        message_params.merge!(group_id: num)
-        group = build(:group, id: num)
-        expect(message.group_id).to eq(group.id)
+        context 'on group_id if there is Group who has a same id' do
+          it 'should be valid' do
+            group = build(:group, id: num)
+            expect(message.group_id).to eq(group.id)
+          end
+        end
       end
     end
 
     # メッセージを保存できない場合
     context 'if a messsage cannot be saved' do
 
-      # メッセージも画像も無いと保存できない
-      it 'is invalid without a body and an image' do
-        message_params.merge!(body: '', image: nil)
-        message.valid?
-        expect(message.errors[:body_or_image]).to include('を入力してください。')
+    #   # メッセージも画像も無いと保存できない
+      context 'without a body and an image' do
+        let(:body) { '' }
+        let(:image) { '' }
+        let(:user_id) { num }
+        let(:group_id) { num }
+        it 'should be invalid' do
+          message.valid?
+          expect(message.errors[:body_or_image]).to include('を入力してください。')
+        end
+      end
+
+    #   # user_idが無いと保存できない
+      context 'without a user_id' do
+        let(:body) { text }
+        let(:image) { url }
+        let(:user_id) { '' }
+        let(:group_id) { num }
+        it 'should be invalid' do
+          message.valid?
+          expect(message.errors[:user_id]).to include('を入力してください。')
+        end
       end
 
       # group_idが無いと保存できない
-      it 'is invalid without a group_id' do
-        message_params.merge!(group_id: '')
-        message.valid?
-        expect(message.errors[:group_id]).to include('を入力してください。')
-      end
-
-      # user_idが無いと保存できない
-      it 'is invalid without a user_id' do
-        message_params.merge!(user_id: '')
-        message.valid?
-        expect(message.errors[:user_id]).to include('を入力してください。')
+      context 'without a group_id' do
+        let(:body) { text }
+        let(:image) { url }
+        let(:user_id) { num }
+        let(:group_id) { '' }
+        it 'should be invalid' do
+          message.valid?
+          expect(message.errors[:group_id]).to include('を入力してください。')
+        end
       end
 
       # 外部キー制約
-        # user_idと同じidを持つユーザーがいないと保存できない
-      it 'is invalid with FOREIGN KEY constraint on user_id' do
-        message_params.merge!(user_id: 2)
-        user = build(:user, id: 3)
-        message.valid?
-        expect(message.user_id).not_to eq(user.id)
-      end
+      context 'with FOREIGN KEY constraint' do
+        let(:body) { text }
+        let(:image) { url }
+        let(:user_id) { 2 }
+        let(:group_id) { 2 }
+          # user_idと同じidを持つユーザーがいないと保存できない
+        context 'on user_id unless there is User who has a same id' do
+          it 'should be invalid' do
+            user = build(:user, id: 3)
+            message.valid?
+            expect(message.user_id).not_to eq(user.id)
+          end
+        end
 
-        # group_idと同じidを持つユーザーがいないと保存できない
-      it 'is invalid with FOREIGN KEY constraint on group_id' do
-        message_params.merge!(group_id: 2)
-        group = build(:user, id: 3)
-        message.valid?
-        expect(message.group_id).not_to eq(group.id)
+          # group_idと同じidを持つユーザーがいないと保存できない
+        context 'on group_id unless there is Group who has a same id' do
+          it 'should be invalid' do
+            group = build(:user, id: 3)
+            message.valid?
+            expect(message.group_id).not_to eq(group.id)
+          end
+        end
       end
     end
   end
