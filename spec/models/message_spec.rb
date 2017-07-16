0411a4f6099869a53af0e0c868e833489b121932
安達和史 <kazu40808@gmail.com>
2017-07-16 10:56:49 +0900

    remove foreign_key test

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -41,27 +41,6 @@ describe Message do
           is_expected.to be_valid
         end
       end
-
-      context 'with FOREIGN KEY constraint' do
-        let(:body) { text }
-        let(:image) { url }
-        let(:user_id) { num }
-        let(:group_id) { num }
-
-        context 'on user_id if there is User who has a same id' do
-          it 'should be valid' do
-            user = build(:user, id: num)
-            expect(message.user_id).to eq(user.id)
-          end
-        end
-
-        context 'on group_id if there is Group who has a same id' do
-          it 'should be valid' do
-            group = build(:group, id: num)
-            expect(message.group_id).to eq(group.id)
-          end
-        end
-      end
     end
 
     context 'if a messsage cannot be saved' do
@@ -96,28 +75,6 @@ describe Message do
         it 'should be invalid' do
           message.valid?
           expect(message.errors[:group_id]).to include('を入力してください。')
-        end
-      end
-
-      context 'with FOREIGN KEY constraint' do
-        let(:body) { text }
-        let(:image) { url }
-        let(:user_id) { 2 }
-        let(:group_id) { 2 }
-        context 'on user_id unless there is User who has a same id' do
-          it 'should be invalid' do
-            user = build(:user, id: 3)
-            message.valid?
-            expect(message.user_id).not_to eq(user.id)
-          end
-        end
-
-        context 'on group_id unless there is Group who has a same id' do
-          it 'should be invalid' do
-            group = build(:user, id: 3)
-            message.valid?
-            expect(message.group_id).not_to eq(group.id)
-          end
         end
       end
     end
