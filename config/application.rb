2afe1183e2b6314e37bc1cfa1a0c456db1cb3cd3
安達和史 <kazu40808@gmail.com>
2017-07-29 22:04:06 +0900

    hide deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+ActiveSupport::Deprecation.silenced = true
+
 module ChatSpace
   class Application < Rails::Application
     config.generators do |g|
