45de71312fa7395968f5252209f65bcee3fe2b06
安達和史 <kazu40808@gmail.com>
2017-07-29 11:39:02 +0900

    create message_cotroller_spec.rb

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe MessagesController, type: :controller do
+  let(:user) { create(:user) }
+  let(:group) { create(:group) }
+  let(:message) { create(:message) }
+
+
+end
