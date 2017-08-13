ce20f35b9c96e0a47374883cb045e5bb99b5ddea
安達和史 <kazu40808@gmail.com>
2017-08-13 20:48:10 +0900

    build remove button in pushing add button

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -8,6 +8,13 @@ $(document).on('turbolinks:load', function() {
     $('#user-search-result').append(html);
   }
 
+  function buildRemoveButton(user) {
+    var html = `<div class='chat-group-user clearfix'>
+                  <p class='chat-group-user__name'>${user.name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove' data-user-id='${user.id}' data-user-name='${user.name}'>削除</a>
+                </div>`;
+    $('#registered').append(html);
+  }
 
   $('#user-search-field').on('keyup', function() {
     $('#user-search-result').children().remove();
@@ -29,5 +36,12 @@ $(document).on('turbolinks:load', function() {
     .fail(function() {
       alert('検索に失敗しました。')
     })
-  });
+  })
+
+  $(document).on('click', '.chat-group-user__btn--add', function(e) {
+    e.preventDefault();
+    var user = {name: $(this).attr('data-user-name'), id: $(this).attr('data-user-id')}
+    buildRemoveButton(user);
+    $(this).parent("div").remove();
+  })
 });
