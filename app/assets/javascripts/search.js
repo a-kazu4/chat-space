4b54bdf052f4cf1c3108b5e02ca724a0cd78a939
安達和史 <kazu40808@gmail.com>
2017-08-14 19:35:27 +0900

    modify search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,9 +1,67 @@
 $(document).on('turbolinks:load', function() {
 
+  IncrementalSearch();
+  clickAddButton();
+  clickRemoveButton();
+
+  function IncrementalSearch() {
+    $('#user-search-field').on('keyup', function() {
+      removeElementUnderSearchResult();
+      var input = $('#user-search-field').val();
+      $.ajax({
+        url:      '/users/search',
+        type:     'GET',
+        data:     {keyword: input},
+        datatype: 'json'
+      })
+      .done(function(users) {
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            buildAddButton(user);
+          });
+        }
+        $('#user-search-field').val();
+      })
+      .fail(function() {
+        alert('検索に失敗しました。')
+      })
+    })
+  }
+
+  function clickAddButton() {
+    $(document).on('click', '.chat-group-user__btn--add', function(e) {
+      stopActionOfPushingButton(e);
+      var aTagWithButton = this;
+      var user = setUser(aTagWithButton);
+      removePreviousButton(aTagWithButton);
+      buildRemoveButton(user);
+    })
+  }
+
+  function clickRemoveButton() {
+    $(document).on('click', '.chat-group-user__btn--remove', function(e) {
+      stopActionOfPushingButton(e);
+      var aTagWithButton = this;
+      var user = setUser(aTagWithButton);
+      removePreviousButton(aTagWithButton);
+      buildAddButton(user);
+    })
+  }
+
   function buildAddButton(user) {
+    var getCurrentUserId = $('#get_current_user_id').attr('value');
+    var getUserId = $('#get_user_id').attr('value');
+    if (getCurrentUserId == user.id) {
+      var addButton = '';
+    } else if (getUserId == user.id) {
+      var addButton = '';
+    } else {
+      var addButton = `<a class='user-search-add chat-group-user__btn chat-group-user__btn--add'>追加</a>`;
+    }
     var html = `<div class='chat-group-user clearfix'>
                   <p class='chat-group-user__name'>${user.name}</p>
-                  <a class='user-search-add chat-group-user__btn chat-group-user__btn--add' data-user-id='${user.id}' data-user-name='${user.name}'>追加</a>
+                  ${addButton}
+                  <input type='hidden' name='group[user_ids][]' value='${user.id}'></input>
                 </div>`;
     $('#user-search-result').append(html);
   }
@@ -11,44 +69,27 @@ $(document).on('turbolinks:load', function() {
   function buildRemoveButton(user) {
     var html = `<div class='chat-group-user clearfix'>
                   <p class='chat-group-user__name'>${user.name}</p>
-                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove' data-user-id='${user.id}' data-user-name='${user.name}'>削除</a>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove'>削除</a>
+                  <input type='hidden' name='group[user_ids][]' value='${user.id}' id='get_user_id'></input>
                 </div>`;
-    $('#registered').append(html);
+    $('#registered_user').append(html);
   }
 
-  $('#user-search-field').on('keyup', function() {
+  function removeElementUnderSearchResult() {
     $('#user-search-result').children().remove();
-    var input = $('#user-search-field').val();
-    $.ajax({
-      url:      '/users/search',
-      type:     'GET',
-      data:     {keyword: input},
-      datatype: 'json'
-    })
-    .done(function(users) {
-      if (users.length !== 0) {
-        users.forEach(function(user) {
-          buildAddButton(user);
-        });
-      }
-      $('#user-search-field').val();
-    })
-    .fail(function() {
-      alert('検索に失敗しました。')
-    })
-  })
+  };
 
-  $(document).on('click', '.chat-group-user__btn--add', function(e) {
-    e.preventDefault();
-    var user = {name: $(this).attr('data-user-name'), id: $(this).attr('data-user-id')};
-    buildRemoveButton(user);
-    $(this).parent("div").remove();
-  })
+  function stopActionOfPushingButton(event) {
+    event.preventDefault();
+  };
 
-  $(document).on('click', '.chat-group-user__btn--remove', function(e) {
-    e.preventDefault();
-    var user = {name: $(this).attr('data-user-name'), id: $(this).attr('data-user-id')};
-    buildAddButton(user);
-    $(this).parent('div').remove();
-  })
+  function setUser(aTagWithButton) {
+    var user = {name: $(aTagWithButton).parent().find('p').html(), id: $(aTagWithButton).parent().find('input').attr('value')};
+    return user
+  };
+
+  function removePreviousButton(aTagWithButton) {
+    $(aTagWithButton).parent('div').remove();
+  }
+
 });
