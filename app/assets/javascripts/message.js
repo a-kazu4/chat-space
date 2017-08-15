6ec4316f1219bf7bb10ed30865fcfa71b8a68b06
安達和史 <kazu40808@gmail.com>
2017-08-15 13:21:04 +0900

    create automaticUpdating();

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,30 @@
 $(document).on('turbolinks:load', function() {
 
   asynchronousCommunication();
+  automaticUpdating();
+
+  function automaticUpdating() {
+    var pathOfThisPage = location.pathname;
+    var group_id = $('#get_group_id').attr('data_group_id');
+    if (pathOfThisPage == '/group/' + group_id + '/messages') {
+      var urlOfThisPage = location.href;
+      $.ajax({
+        type: 'GET',
+        url: urlOfThisPage,
+        dataType: 'json'
+      })
+      .done(function(messages) {
+        messages.forEach(function(message) {
+          buildHTML(message);
+        });
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました')
+      })
+    } else {
+      clearInterval(interval);
+    }
+  };
 
   function asynchronousCommunication() {
     $('#new_message_form').on('submit', function(e) {
@@ -39,7 +63,7 @@ $(document).on('turbolinks:load', function() {
       var chatBottom = insertBodyAndImage;
     }
 
-    var html = `<div class='chat__body__chat'>
+    var html = `<div class='chat__body__chat' data-message-id="${message.id}">
                   <div class='chat__body__chat__top'>
                     <div class='chat__body__chat__top__name'>${message.name}</div>
                     <div class='chat__body__chat__top__time'>${message.time}</div>
@@ -51,6 +75,8 @@ $(document).on('turbolinks:load', function() {
 
     $('.chat__body').append(html);
   }
+
+  var interval = setInterval(automaticUpdating(), 1000 * 5);
 
   function stopActionOfPushingSendButton(event) {
     event.preventDefault();
